8f2a2b8a0a60a455ff3e1678f043360812815d05
soadam <soad.anam7@gmail.com>
2022-04-04 22:51:32 +0600

    added active nav feature

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,11 +4,18 @@ import { NavLink } from 'react-router-dom';
 
 
 const Navigation = () => {
+
+     let activeStyle = {
+         textDecoration: 'underline',
+         color: 'red',
+         fontWeight: 'bold',
+     };
+
     return (
         <nav>
-            <NavLink to="/home" className='nav-item'>Home</NavLink>
-            <NavLink to="/restaurant" className='nav-item'>Restaurant</NavLink>
-            <NavLink to="/home/about" className='nav-item'>About Us</NavLink>
+            <NavLink to="/home/" className='nav-item' style={({isActive}) => isActive ? activeStyle : undefined}>Home</NavLink>
+            <NavLink to="/restaurant" className='nav-item' style={({isActive}) => isActive ? activeStyle : undefined}>Restaurant</NavLink>
+            <NavLink to="/home/about" className='nav-item' style={({isActive}) => isActive ? activeStyle : undefined}>About Us</NavLink>
         </nav>
     );
 };
